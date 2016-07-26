2e32ce6005cc5c6d1efe3044dbb25ae2bcc0a8ab
Corey Daniels <cdouglasd@gmail.com>
2016-07-25 20:36:01 -0600

    Add basic world space UI

---
+++ Assets/GazeDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GazeDisplay : MonoBehaviour {
+
+    private CardboardHead head;
+    private Text gazeText;
+
+	void Start() {
+        head = FindObjectOfType<CardboardHead>();
+        gazeText = GetComponentInChildren<Text>();
+	}
+	
+	void Update() {
+        gazeText.text = head.Gaze.ToString();
+	}
+}
