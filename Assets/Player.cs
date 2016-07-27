0536b55813e2ef44756e47bd26013b2e25cf8cb5
Corey Daniels <cdouglasd@gmail.com>
2016-07-26 19:31:33 -0600

    Add jump ability

---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player: MonoBehaviour {
+
+	public int jumpVelocity = 5;
+	public float jumpAngle = 45f;
+
+    private CardboardHead head;
+    private Text gazeText;
+	private Rigidbody rb;
+
+	private bool onFloor;
+
+	void Start() {
+		Cardboard.SDK.OnTrigger += PullTrigger;
+        head = GetComponentInChildren<CardboardHead>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void OnCollisionEnter() {
+		onFloor = true;
+	}
+
+    private void PullTrigger() {
+		if (onFloor) {
+			onFloor = false;
+			Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+			Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngle * Mathf.Deg2Rad, 0);
+			rb.velocity = jumpVector * jumpVelocity;
+		}
+    }
+	
+}
