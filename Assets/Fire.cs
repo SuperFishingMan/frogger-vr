09c7d3efd7dd92fe2333f0d2fecf39a8686651cd
Corey Daniels <cdouglasd@gmail.com>
2016-07-28 18:42:21 -0600

    Add fire wall

---
+++ Assets/Fire.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	private Player player;
+
+	void Start() {
+		player = FindObjectOfType<Player>();
+	}
+	
+	void Update() {
+		Vector3 delta = player.transform.position - transform.position;
+		Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
+		transform.Translate(projectedDelta);
+	}
+}
