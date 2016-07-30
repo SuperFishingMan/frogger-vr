49e2b19e3afc5cfd141ce330dfee1c40b96aa119
Corey Daniels <cdouglasd@gmail.com>
2016-07-29 20:12:48 -0600

    Add in-game menus

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	public void ResetGame() {
+		SceneManager.LoadScene("Main");
+	} 
+
+	public void BackToMenu() {
+		SceneManager.LoadScene("SplashScreen");
+	}
+
+}
