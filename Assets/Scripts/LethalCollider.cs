4e0f29b513c25bb440c0b2cf5ed485ac2adbaebc
Corey Daniels <cdouglasd@gmail.com>
2016-08-01 17:02:20 -0600

    End game on vehicle collision

---
+++ Assets/Scripts/LethalCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LethalCollider : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.GetComponent<Player>() != null) {
+			GameState gameState = FindObjectOfType<GameState>();
+			gameState.IsGameOver = true;
+		}
+	}
+
+}
