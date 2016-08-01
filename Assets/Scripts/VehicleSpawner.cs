431e57163d71d79aa59d049ec59bdd265ab36d13
Corey Daniels <cdouglasd@gmail.com>
2016-07-31 20:25:08 -0600

    Set up vehicle spawning

---
+++ Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+	public Vehicle[] vehiclePrefabs;
+	public float startPosition = -10f;
+	public float laneSpeed = 5f;
+	
+	void Start() {
+		SpawnVehicle();
+	}
+
+	void SpawnVehicle() {
+		Vehicle newVehicle = Instantiate(vehiclePrefabs[0]);
+		newVehicle.transform.position = GetPositionOffest();
+		newVehicle.transform.parent = transform;
+		newVehicle.SetSpeed(laneSpeed);
+	}
+
+	Vector3 GetPositionOffest() {
+		float spawnHeight = (transform.localScale.y / 2) + (vehiclePrefabs[0].transform.localScale.y / 2);
+		return new Vector3(startPosition, spawnHeight, transform.position.z);
+	}
+}
---
+++ Assets/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+
+	private float speed;
+	
+	void Update () {
+		transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+
+	public void SetSpeed(float newSpeed) {
+		speed = newSpeed;
+	}
+}
